073e756a3565a2bf4801a10970f96d7b6fe7ebc6
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-07 21:10:17 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace T01.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Stack stack = new Stack();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                stack.Push(word[i]);
+            }
+            for (int i = stack.Count; i > 0; i--)
+            {
+                Console.Write(stack.Peek());
+                stack.Pop();
+            }
+        }
+    }
+}
+   
