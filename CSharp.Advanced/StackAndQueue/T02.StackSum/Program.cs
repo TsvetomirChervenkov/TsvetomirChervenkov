474184274fe07188383354ee5f4ecb6d2d28638b
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-11 11:19:46 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T02.StackSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(input);
+            string commands = string.Empty;
+            while(commands != "end")
+            {
+                commands = Console.ReadLine().ToLower();
+                string[] command = commands.Split().ToArray();  
+                string commandArgs = command[0].ToLower();
+                if (commandArgs == "add")
+                {
+                    int first = int.Parse(command[1]);
+                    int second = int.Parse(command[2]);
+                    stack.Push(first);
+                    stack.Push(second);
+                }
+                if (commandArgs == "remove")
+                {
+                    int count = int.Parse(command[1]);
+                    if (count> stack.Count)
+                    {
+                        
+                        continue;
+                    }
+                    for (int i = 0; i < count; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            int sum = 0;
+            foreach (var item in stack)
+            {
+                sum += item;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
