412eafe62ee76528ab6dcba76d31c5bff13b6620
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-12 10:31:23 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T04.Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04.Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack <int> indices = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {               
+                char current = input[i];
+                if (current == '(')
+                {
+                    indices.Push(i);
+                }
+                if (current == ')')
+                {
+                    Console.WriteLine($"{input.Substring(indices.Peek(), 1+ i - indices.Peek())}");
+                    indices.Pop();
+                }
+            }
+        }
+    }
+}
