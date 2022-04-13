b89654b8c314e56784951030531c79b95383e0fe
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-13 09:58:20 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T06.Supermarket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Queue<string> queue = new Queue<string>();
+
+            while ((input = Console.ReadLine())!= "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count != 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                    continue;
+                }
+                queue.Enqueue(input);
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
