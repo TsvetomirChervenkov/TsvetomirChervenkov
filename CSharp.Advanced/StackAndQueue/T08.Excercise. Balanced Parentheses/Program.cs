05d710ded1bddd40b86d6183fcfaf26efcb02852
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-14 12:57:46 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T08.Excercise. Balanced Parentheses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08.Excercise._Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] paranthesis = Console.ReadLine().ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            if (paranthesis.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            for (int i = 0; i < paranthesis.Length; i++)
+            {
+                char current = paranthesis[i];
+                int asciiChar = (int)current;
+                if (current == '(' || current == '{' || current == '[')
+                {
+                    stack.Push(current);
+                }
+                else if (asciiChar == (int)stack.Peek()+2 || asciiChar == (int)stack.Peek()+1)
+                {
+                    stack.Pop();
+                }
+                else
+                {   
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+            Console.WriteLine("YES");
+        }
+    }
+}
