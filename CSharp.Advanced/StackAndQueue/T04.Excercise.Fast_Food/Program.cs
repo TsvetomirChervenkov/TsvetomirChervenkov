9ae1e44acd2ad154b7f837237dab000f7d3bb498
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-17 18:25:53 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T04.Excercise.Fast_Food/Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04.Excercise.Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDishes = int.Parse(Console.ReadLine());
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+           
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count != 0)
+            {
+                if (numberOfDishes >= orders.Peek())
+                {
+                    numberOfDishes -= orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Orders left: " + String.Join(" ", orders));
+                    return;
+                }
+            }
+            Console.WriteLine($"Orders complete");
+        }
+    }
+}
