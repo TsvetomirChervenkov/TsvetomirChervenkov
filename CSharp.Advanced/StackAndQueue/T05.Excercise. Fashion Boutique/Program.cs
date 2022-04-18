f2b99ddee6b988d0fbf3b2f618d3409f7e834432
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-04-18 19:09:30 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T05.Excercise. Fashion Boutique/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.Excercise._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rackCapacity = int.Parse(Console.ReadLine());
+            Stack<int> stackOfClothes = new Stack<int>(clothes);
+            int rackCounter = 0;
+            while (stackOfClothes.Count != 0)
+            {
+                int currentRack = rackCapacity;     
+                rackCounter++;
+                while (currentRack > 0)
+                {            
+                    if (stackOfClothes.Count == 0 || currentRack - stackOfClothes.Peek() < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        currentRack -= stackOfClothes.Pop();
+                    }
+                }
+            }
+            Console.WriteLine(rackCounter);
+        }
+    }
+}
