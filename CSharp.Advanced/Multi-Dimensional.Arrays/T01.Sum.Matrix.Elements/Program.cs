d9af7aafdeb30df9d0b4daf7b0f73d0412f516d8
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-05-03 22:17:32 +0300

    Add files via upload

---
+++ CSharp.Advanced/Multi-Dimensional.Arrays/T01.Sum.Matrix.Elements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T01_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+            for (int row = 0; row < matrixSize[0]; row++)
+            {
+                int[] firstRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int column = 0; column < matrixSize[1]; column++)
+                {
+                    matrix[row, column] = firstRow[column];
+                }
+            }
+            Console.WriteLine(matrixSize[0]);
+            Console.WriteLine(matrixSize[1]);
+            int sum = 0;
+            foreach (var digit in matrix)
+            {
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
