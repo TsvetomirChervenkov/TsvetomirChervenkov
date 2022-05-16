1814e37ee3c8dc898c23b19daeed7069737c69eb
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-05-16 20:45:04 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T03.Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split().ToArray();
+            Array.Reverse(array);
+            Stack<string> stack = new Stack<string>(array);
+            int result = int.Parse(stack.Pop());
+
+            while (stack.Count != 0)
+            {
+                string currentOperator = stack.Pop();
+                if (currentOperator == "-")
+                {
+                    int currentDigit = int.Parse(stack.Pop());
+                    result -= currentDigit;
+                }
+                if (currentOperator == "+")
+                {
+                    int currentDigit = int.Parse(stack.Pop());
+                    result += currentDigit;
+                }             
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
