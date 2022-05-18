59f3f19aca2af28ff1a37291b459c4b5af19efed
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-05-18 19:38:21 +0300

    Add files via upload

---
+++ CSharp.Advanced/Multi-Dimensional.Arrays/T03.Primary Diagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace T03.Primary_Diagona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubeSize = int.Parse(Console.ReadLine());
+            int[,] cube = new int[cubeSize, cubeSize];
+            for (int row = 0; row < cubeSize; row++)
+            {
+                int[] rowCube = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int column = 0; column < cubeSize; column++)
+                {
+                    cube[row, column] = rowCube[column];
+                }
+            }
+            int sum = 0;
+            for (int row = 0; row < cubeSize; row++)
+            {
+                for (int column = 0; column < cubeSize; column++)
+                {
+                    if (row == column)
+                    {
+                        sum += cube[row, column];
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
