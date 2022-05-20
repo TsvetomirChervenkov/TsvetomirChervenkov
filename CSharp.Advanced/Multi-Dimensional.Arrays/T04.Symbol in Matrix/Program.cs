f13f6d512493d418960f6f25ce42ef1404a5224f
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-05-20 20:45:29 +0300

    Add files via upload

---
+++ CSharp.Advanced/Multi-Dimensional.Arrays/T04.Symbol in Matrix/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T04.Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cubeSize = int.Parse(Console.ReadLine());
+            char[,] cube = new char[cubeSize, cubeSize];
+            for (int row = 0; row < cubeSize; row++)
+            {
+                string rowCube = Console.ReadLine();
+                char[] chars = rowCube.ToCharArray();
+                for (int column = 0; column < cubeSize; column++)
+                {
+                    cube[row, column] = chars[column];
+                }
+            }
+            char searchedChar = char.Parse(Console.ReadLine());
+
+            for (int row = 0; row < cubeSize; row++)
+            {          
+                for (int column = 0; column < cubeSize; column++)
+                {
+                    if (searchedChar == cube[row,column])
+                    {
+                        Console.WriteLine($"({row}, {column})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{searchedChar} does not occur in the matrix");
+        }
+    }
+}
