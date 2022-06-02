94de5079bd4e5467b7a844aef4289c779d75e66c
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-06-02 18:11:19 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T05.PrintEvenSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.PrintEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
