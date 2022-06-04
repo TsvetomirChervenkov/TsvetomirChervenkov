0da50748961839c0fece3b0da6441bc8efac275d
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-06-04 11:08:55 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T08.Traffic Jam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08.Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passingCars = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int counter = 0;
+
+            Queue<string> cars = new Queue<string>();
+
+            while ((input = Console.ReadLine())!= "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < passingCars; i++)
+                    {
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        counter++;
+                    }
+                    continue;
+                }
+                cars.Enqueue(input);
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
