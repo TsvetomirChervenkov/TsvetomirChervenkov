ac762936bd3d52ceb99ea872757a086285008343
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-06-04 11:07:45 +0300

    Add files via upload

---
+++ CSharp.Advanced/StackAndQueue/T08.Traffic Jam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split().ToList();
+            int nth = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>(input);
+
+            int counter = 1;
+
+            while (input.Count != 1)
+            {
+                string currentName = queue.Peek();
+                if (counter == nth)
+                {
+                    queue.Dequeue();
+                    input.Remove(currentName);
+                    counter = 1;
+                    Console.WriteLine($"Removed {currentName}");
+                    continue;
+                }
+                queue.Dequeue();
+                queue.Enqueue(currentName);
+                
+               
+                counter++;
+            }
+            Console.WriteLine("Last is " + String.Join(" ", queue));
+        }
+    }
+}
