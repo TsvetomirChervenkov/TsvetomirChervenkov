dc22d303e13e81a12125676e7169bf6f884ca73f
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-06-12 18:38:53 +0300

    Update Program.cs

--- CSharp.Advanced/StackAndQueue/T06.Excercise.Songs Queue/Program.cs
+++ CSharp.Advanced/StackAndQueue/T06.Excercise.Songs Queue/Program.cs
@@ -1,170 +1,44 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Sandbox
+namespace T06._Songs_Queue
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your Mathematical equation with singlespace between every symbol:");
-            string input = Console.ReadLine();
+            Queue<string> playlist = new Queue<string>(Console.ReadLine().Split(", "));
 
-            string[] tokenList = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-            Queue<string> outputQueue = new Queue<string>();
-            Stack<char> operatorStack = new Stack<char>();
-
-            for (int i = 0; i < tokenList.Length; i++)
+            while (playlist.Count > 0)
             {
-                string currentToken = tokenList[i];
+                string[] command = Console.ReadLine().Split(' ').ToArray();
 
-                if (currentToken.Length > 1)
+                if (command[0] == "Play")
                 {
-                    outputQueue.Enqueue(currentToken);
-                    continue;
+                    playlist.Dequeue();
                 }
-
-                char currentElement = char.Parse(currentToken);
-                bool isNumber = char.IsDigit(currentElement);
-
-                if (isNumber)
+                else if (command[0] == "Add")
                 {
-                    outputQueue.Enqueue(currentElement.ToString());
-                    continue;
-                }
+                    string songToAdd = string.Join(" ", command.Skip(1));
 
-                if (currentElement == '(')
-                {
-                    operatorStack.Push(currentElement);
-                    continue;
-                }
-
-                if (currentElement == ')')
-                {
-                    char topOperator = operatorStack.Peek();
-                    while (topOperator != '(')
+                    if (playlist.Contains(songToAdd))
                     {
-                        string currentOperator = operatorStack.Pop().ToString();
-                        outputQueue.Enqueue(currentOperator);
-                        topOperator = operatorStack.Peek();
+                        Console.WriteLine($"{songToAdd} is already contained!");
                     }
-                    operatorStack.Pop();
-                    continue;
+                    else
+                    {
+                        playlist.Enqueue(songToAdd);
+                    }
                 }
-
-                if (operatorStack.Count == 0)
+                else if (command[0] == "Show")
                 {
-                    operatorStack.Push(currentElement);
-                    continue;
-                }
-
-                char previousOperator = operatorStack.Peek();
-                bool isTopSymbolGreater = IsGreaterPrecedence(previousOperator, currentElement);
-
-                while (isTopSymbolGreater)
-                {
-                    string currentOperator = operatorStack.Pop().ToString();
-                    outputQueue.Enqueue(currentOperator);
-                }
-
-                operatorStack.Push(currentElement);
-            }
-
-            while (operatorStack.Count > 0)
-            {
-                string oper = operatorStack.Pop().ToString();
-                outputQueue.Enqueue(oper);
-            }
-
-
-            Stack<decimal> resultStack = new Stack<decimal>();
-            decimal result = 0;
-
-            while (outputQueue.Count > 0)
-            {
-                string currentQueueElement = outputQueue.Dequeue();
-                if (currentQueueElement.Length > 1)
-                {
-                    resultStack.Push(decimal.Parse(currentQueueElement));
-                    continue;
-                }
-
-                char elementToChar = char.Parse(currentQueueElement);
-                if (char.IsDigit(elementToChar))
-                {
-                    resultStack.Push(decimal.Parse(currentQueueElement));
-                    continue;
-                }
-
-                char symbol = elementToChar;
-                decimal secondNumber = resultStack.Pop();
-                decimal firstNumber = resultStack.Pop();
-                result = Calculate(symbol, firstNumber, secondNumber);
-                resultStack.Push(result);
-
-            }
-            Console.WriteLine(string.Join(" ", resultStack));
-        }
-
-        private static decimal Calculate(char symbol, decimal firstNumber, decimal secondNumber)
-        {
-            decimal result = 0;
-
-            if (symbol == '+')
-            {
-                result = firstNumber + secondNumber;
-            }
-            else if (symbol == '-')
-            {
-                result = firstNumber - secondNumber;
-            }
-            else if (symbol == '*')
-            {
-                result = firstNumber * secondNumber;
-            }
-            else if (symbol == '/')
-            {
-                if (firstNumber == 0 || secondNumber == 0)
-                {
-                    Console.WriteLine("Cannot divide by 0");
-                }
-                else
-                {
-                    result = firstNumber / secondNumber;
+                    Console.WriteLine(string.Join(", ", playlist));
                 }
             }
-            return result;
-        }
 
-        private static bool IsGreaterPrecedence(char symbolToCompare, char otherSymbol)
-        {
-            bool isIsGreaterPrecedence = false;
-
-            int firstChar = GetPrecedence(symbolToCompare);
-            int secondChar = GetPrecedence(otherSymbol);
-
-            if (firstChar > secondChar)
-            {
-                isIsGreaterPrecedence = true;
-            }
-
-            return isIsGreaterPrecedence;
-        }
-
-        private static int GetPrecedence(char symbol)
-        {
-            int precedence = 0;
-            if (symbol == '+' || symbol == '-')
-            {
-                precedence = 1;
-            }
-            else if (symbol == '*' || symbol == '/')
-            {
-                precedence = 2;
-            }
-
-            return precedence;
+            Console.WriteLine("No more songs!");
         }
     }
 }
