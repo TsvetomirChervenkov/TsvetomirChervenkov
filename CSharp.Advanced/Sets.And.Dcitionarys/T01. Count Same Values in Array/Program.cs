9905f19244f8462521f0c5b7a80f2eacca029a58
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-06-24 12:46:10 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T01. Count Same Values in Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            Dictionary<double, int> occurances = new Dictionary<double, int>();
+
+            foreach (var digit in input)
+            {
+                if (!occurances.ContainsKey(digit))
+                    occurances.Add(digit, 0);
+                occurances[digit]++;
+            }
+
+            foreach (var kvp in occurances)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
