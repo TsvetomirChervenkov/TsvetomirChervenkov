588b8c2ef8e251f4a1e5b369169db1b553ceff1b
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-07-01 22:04:22 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T01.Excercise.Unique Usernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T01.Excercise.Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+
+            for (int row = 0; row < inputLines; row++)
+                uniqueUsernames.Add(Console.ReadLine());      
+            
+            foreach (var username in uniqueUsernames)
+                Console.WriteLine(username);            
+        }
+    }
+}
