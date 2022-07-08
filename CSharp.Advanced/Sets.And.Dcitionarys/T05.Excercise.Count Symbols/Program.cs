5d062c3f746637451fb6c6a1c113769a1fce3002
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-07-08 22:07:20 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T05.Excercise.Count Symbols/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T05.Excercise.Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+
+            SortedDictionary<char, int> symbols = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!symbols.ContainsKey(input[i]))
+                {
+                    symbols.Add(input[i], 0);
+                }
+                symbols[input[i]]++;
+            }
+            foreach (var character in symbols)
+            {
+                Console.WriteLine($"{character.Key}: {character.Value} time/s");
+            }
+        }
+    }
+}
