93aecad38aaa4e54666a8b354ec5282b048fd3d1
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-08-02 16:42:50 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T02. Excercise.SetsofElements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02._Excercise.Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] setDimensions = Console.ReadLine().Split().Select(decimal.Parse).ToArray();
+            decimal n = setDimensions[0];
+            decimal m = setDimensions[1];
+            HashSet<decimal> nSet = new HashSet<decimal>();
+            
+            HashSet<decimal> uniqueNums = new HashSet<decimal>();
+
+            for (int i = 0; i < n+m; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                if (i< n)
+                {
+                    nSet.Add(num);
+                }
+                if (i >= n)
+                {
+                    uniqueNums.Add(num);
+                }
+            }
+            nSet.IntersectWith(uniqueNums);
+            Console.WriteLine(String.Join(" ", nSet));
+        }
+    }
+}
