f53913ef13fca7d98f31b7e12b8601ab3410d795
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-08-08 09:51:28 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T03.Excercise.Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.Excercise.Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueChemicals = new HashSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] inputData = Console.ReadLine().Split(' ');
+                foreach (var item in inputData)
+                {
+                    uniqueChemicals.Add(item);
+                }
+            }
+            foreach (var item in uniqueChemicals.OrderBy(x=> x))
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
