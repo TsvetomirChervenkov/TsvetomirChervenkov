c63462b4c5a496e3a86c3ee52272a21e7452cdff
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-08-09 12:46:38 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T04. Excercise.EvenTimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04._Excercise.EvenTimes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            Dictionary<int,int> map = new Dictionary<int, int>();   
+
+            for (int i = 0; i < lines; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!map.ContainsKey(num))
+                {
+                    map.Add(num, 0);
+                }
+                map[num]++;
+            }
+            foreach (var item in map)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+            
+            
+        }
+    }
+}
