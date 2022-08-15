05ace4d0a5c4e45e3cc42478aecee93b49859ce5
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-08-15 10:53:54 +0300

    Add files via upload

---
+++ CSharp.Advanced/Sets.And.Dcitionarys/T06.Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06.Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int names = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            for (int inputRow = 0; inputRow < names; inputRow++)
+            {
+                string inputLine = Console.ReadLine();
+                uniqueNames.Add(inputLine);
+            }
+
+            foreach (var item in uniqueNames)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
