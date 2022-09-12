4c4607998f5cc07990b813c1a7bb138f7351c879
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-09-12 10:34:57 +0300

    Add files via upload

---
+++ CSharp.Advanced/StreamsAndFilesandDirectoriesLab/T01.OddLines/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace OddLines 
+{
+    public class OddLines
+    {
+        static void Main() 
+        { 
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+            ExtractOddLines(inputFilePath, outputFilePath);
+
+        }
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StreamWriter writer = new StreamWriter(outputFilePath);
+            int counter = 0;
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                counter++;
+                if (counter %2 != 0)
+                {
+                    writer.WriteLine(line);
+                }              
+            }
+            reader.Close();
+            writer.Close();
+        } 
+    } 
+}
