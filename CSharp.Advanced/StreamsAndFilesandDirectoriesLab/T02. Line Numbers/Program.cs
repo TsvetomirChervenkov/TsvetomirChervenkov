13a118712c9f83ae3001f8c14a6d7dddcc2f17d0
Tsvetomir Chervenkov <102176292+TsvetomirChervenkov@users.noreply.github.com>
2022-09-13 11:12:34 +0300

    Add files via upload

---
+++ CSharp.Advanced/StreamsAndFilesandDirectoriesLab/T02. Line Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ReTakeLineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputPath, string outputPath)
+        {
+            StreamReader reader = new StreamReader(inputPath);
+            StreamWriter writer = new StreamWriter(outputPath);
+
+            int counter = 0;
+            while (!reader.EndOfStream)
+            {
+                counter++;
+                string line = reader.ReadLine();
+                writer.WriteLine($"{counter}. {line}");
+            }
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
